968e1d36e97208a532fdee073e31d243358e9e81
Abdallah-Azme <abdallahazme199794@gmail.com>
2024-04-13 05:16:05 +0200

    add footer and themes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import About from "@/components/About";
 import Contact from "@/components/Contact";
-import Experience from "@/components/Experince";
 import Intro from "@/components/Intro";
 import Projects from "@/components/Projects";
 import SectionDivider from "@/components/SectionDivider";
@@ -14,7 +13,6 @@ export default function Home() {
       <About />
       <Projects />
       <Skills />
-      <Experience />
       <Contact />
     </main>
   );
